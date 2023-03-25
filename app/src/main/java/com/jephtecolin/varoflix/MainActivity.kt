7db2993153bfc9291c6c4336ff40078fd48b8cb5
Jephte Colin <drakecolinj@gmail.com>
2023-03-24 22:55:25 -0400

    Initial Commit

---
+++ app/src/main/java/com/jephtecolin/varoflix/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.jephtecolin.varoflix
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.jephtecolin.varoflix.ui.theme.VaroflixTheme
+import timber.log.Timber
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            VaroflixTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    VaroflixTheme {
+        Greeting("Android")
+    }
+}
